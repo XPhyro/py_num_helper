b706815cef17a0f613a47f6ef6604f84138b9c34
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-05-12 12:01:33 +0300

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# Simple number helper.
+
+
+import math
+
+
+def is_prime(n):
+    if n == 2:
+        return True
+    if n < 2 or not n % 2:
+        return False
+
+    for i in range(3, math.ceil(math.sqrt(n)) + 1, 2):
+        if not n % i:
+            return False
+
+    return True
+
+
+def prime_factors(n):
+    c = 0
+    while not n % 2:
+        n /= 2
+        c += 1
+
+    l = []
+    if c:
+        l.append((2, c))
+
+    for i in range(3, math.trunc(n) + 1, 2):
+        if not is_prime(i):
+            continue
+
+        c = 0
+        while not n % i:
+            n /= i
+            c += 1
+        if c:
+            l.append((i, c))
+
+    return l
